2c49459b941bc4ab69ce500daa3592184d84ef81
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-05-13 22:27:32 +0000

    feature(ReadAllUsersDTO): Implement ReadAllUsersDTO

--- src/SocialMediaApi.Application/DTOs/UserDTO/ReadAllUsersDTO.cs
+++ src/SocialMediaApi.Application/DTOs/UserDTO/ReadAllUsersDTO.cs
@@ -1,6 +1,12 @@
-﻿namespace SocialMediaApi.Application.Dtos.UserDto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaApi.Application.Dtos.UserDto;
 
 public class ReadAllUsersDTO
 {
+    public string Name { get; set; } = null!;
+    [EmailAddress]
+    public string Email { get; set; } = null!;
 
+    public string Username { get; set; } = null!;
 }
