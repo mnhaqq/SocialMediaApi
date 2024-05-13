ba3d14b0e5bf9464654d29b7eaef6d08eab7790b
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-05-13 22:29:48 +0000

    feature(ReadSingleUserDTO): Implement ReadSingleUserDto

--- src/SocialMediaApi.Application/DTOs/UserDTO/ReadSingleUserDTO.cs
+++ src/SocialMediaApi.Application/DTOs/UserDTO/ReadSingleUserDTO.cs
@@ -1,6 +1,18 @@
-﻿namespace SocialMediaApi.Application.Dtos.UserDto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaApi.Application.Dtos.UserDto;
 
 public class ReadSingleUserDTO
 {
+    public string? Bio { get; set; }
+
+    [Url]
+    public string? ProfilePic { get; set; }
+    public string Name { get; set; } = null!;
+
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    public string Username { get; set; } = null!;
 
 }
