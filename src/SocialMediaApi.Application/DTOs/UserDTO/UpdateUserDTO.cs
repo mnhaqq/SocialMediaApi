b684f9a48efbfe2086ab6919b658578cb021debd
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-05-14 04:13:19 +0000

    feature(UpdateUserDTO): Implement User update Dto

--- src/SocialMediaApi.Application/DTOs/UserDTO/UpdateUserDTO.cs
+++ src/SocialMediaApi.Application/DTOs/UserDTO/UpdateUserDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace SocialMediaApi.Application.Dtos.UserDto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaApi.Application.Dtos.UserDto;
 
 public class UpdateUserDTO
 {
+    public string? Bio { get; set; }
+    [Url]
+    public string? ProfilePic { get; set; }
+    public string Name { get; set; } = null!;
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Username { get; set; } = null!;
 
 }
